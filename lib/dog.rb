25263d87766497f93ef72949959d45a79b32ee6c
Karl Amoguis <ktamoguis@gmail.com>
2018-06-03 09:34:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,6 +54,17 @@ class Dog
     new_dog
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
   def self.find_by_id(id)
     # find the student in the database given a name
     # return a new instance of the Student class
