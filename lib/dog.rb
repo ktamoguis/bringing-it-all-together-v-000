aecd8e7afc4b02235b426ee4ae30c50db7bad607
Karl Amoguis <ktamoguis@gmail.com>
2018-06-03 09:02:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,8 +36,8 @@ class Dog
         VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
-  end 
+  end
 
 end
