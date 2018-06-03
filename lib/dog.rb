ca61ef0e71b1300bda6f78e3b461431884502bb8
Karl Amoguis <ktamoguis@gmail.com>
2018-06-03 09:47:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -83,13 +83,13 @@ class Dog
     if !dog.empty?
       #binding.pry
       dog_data = dog[0]
-      binding.pry
+      #binding.pry
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
     end
     dog
-    binding.pry
+    #binding.pry
   end
 
   def update
